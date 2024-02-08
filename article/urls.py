69b449989bfbcfc1dabb052189462f515f698a72
Dinesh Kumar D <138181353+Dinesh-Diner@users.noreply.github.com>
2024-02-08 18:14:19 +0530

    first

---
+++ article/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+app_name='article'
+
+urlpatterns = [
+    path('',index,name='index'),
+    path('article/<int:pk>/',single_article,name='single_article'),
+    path('article/category/<int:pk>',categorised_article,name='categorised_article'),
+    path('article/create/', create_article, name='create_article'), 
+    path('article/update/<int:pk>/',UpdateArticle.as_view(),name="update_article"),
+    path('article/delete/<int:pk>/',DeleteArticle.as_view(),name="delete_article"),
+]
